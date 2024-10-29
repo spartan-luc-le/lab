d78ded7bb17335a21c7dbdb616f7931b1c6badd9
spartan-lucle <luc.le@c0x12c.com>
2024-10-30 01:12:09 +0700

    revise

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,3 @@
-import { useState } from "react";
-import "./App.css";
-import { ContextDemo } from "./ContextDemo";
-import { ZustandDemo } from "./ZustandDemo";
-
-function App() {
-  const [demo, setDemo] = useState("context");
-
-  return (
-    <>
-      <select className="p-2 rounded-md" onChange={(e) => setDemo(e.currentTarget.value)}>
-        <option value="context" selected>
-          React Context
-        </option>
-        <option value="zustand">Zustand</option>
-      </select>
-
-      {demo === "zustand" && <ZustandDemo />}
-      {demo === "context" && <ContextDemo />}
-    </>
-  );
+export function App() {
+  return <div>App</div>;
 }
-
-export default App;
